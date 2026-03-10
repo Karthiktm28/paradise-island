ee0962f2bef16334715c08760ee5f1c5b36e0691
Karthiktm28 <karthiktm2804@gmail.com>
2026-03-10 23:01:31 +0000

    feat(ui): add animated statistics counters

--- Pages/js/main.js
+++ Pages/js/main.js
@@ -70,3 +70,15 @@ counters.forEach(counter => {
   updateCounter();
 
 });
+// Mobile Menu Toggle
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+if (menuToggle) {
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+
+}
